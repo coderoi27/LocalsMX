d7995ce04d95388d2b94ed5722c23de479c8aa0d
CodeRoi. Programando ideas <coderoi271@gmail.com>
2026-06-19 17:25:16 -0600

    Avance de bloques con CODEX
    Avanzamos con lo que teníamos pendiente con Codex. Eliminamos los .env desde git

--- src/Controller/Public/LandingController.php
+++ src/Controller/Public/LandingController.php
@@ -6,17 +6,15 @@ namespace App\Controller\Public;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class LandingController extends AbstractController
+final class LandingController extends AbstractController
 {
     #[Route('/', name: 'public_landing', methods: ['GET'])]
     public function index(): Response
     {
-        // Si el usuario ya está logueado, redirigir al panel.
-        if ($this->getUser()) {
-            // Nota: ajustar 'owner_dashboard' según la ruta real que se defina luego
-            return $this->redirectToRoute('owner_locations_index');
+        if ($this->getUser() !== null) {
+            return $this->redirectToRoute('locals_dashboard');
         }
 
         return $this->render('public/landing.html.twig');
